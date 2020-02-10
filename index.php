8724fabb8a5adf8ffa5a6082008cee0ce2173a9b
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-02-10 19:31:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-<!DOCTYPE html>
-<html>
-<body onload="loadXMLDoc()">
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xmlfaus.herokuapp.com/rssf.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("music");
+?>
 
-<h2>Using the XMLHttpRequest Object</h2>
+ <h1>Top 5 Music 2019</h1>
 
-<div id="rss">
-</div>
-
-<script>
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("rss").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "rss-cnn.php", true);
-  xhttp.send();
-}
-</script>
-</body>
-</html>
+<?php
+ foreach( $content as $data ){
+     $mus_title = $data->getElementsByTagName("mus_title")->item(0)->nodeValue;
+     $mus_artist = $data->getElementsByTagName("mus_artist")->item(0)->nodeValue;
+     $mus_genre = $data->getElementsByTagName("mus_genre")->item(0)->nodeValue;
+     $mus_writer = $data->getElementsByTagName("mus_writer")->item(0)->nodeValue;
+     echo "<ul>
+            <h2>$mus_title</h2>
+              <ul>
+                  <li>Artist: $mus_artist </li>
+                  <li>Genre: $mus_genre </li>
+                  <li>Writer: $mus_writer </li>
+              </ul>
+          </ul>";
+ }
+?>
